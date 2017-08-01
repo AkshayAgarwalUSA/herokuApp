27fa20b42fe64615fed38e06d42b7b7e4355ebfb
collins <collins@collinss-MacBook-Pro-2.local>
2017-08-01 19:11:41 -0400

    this si a git repo

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+// var path = require('path');
+var socket = require('socket.io');
+// var logger = require('morgan');
+// var bodyParser = require('body-parser');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+// app.use(logger('dev'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+// app.get('/', )
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+var io = socket(server)
+
+io.on('connection', function(socket){
+
+  console.log('socket is there',socket.id);
+
+  socket.on('app', function (data){
+
+    console.log('data', data['input']);
+    socket.emit('cb', data);
+  })
+});
