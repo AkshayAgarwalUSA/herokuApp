6f94863483084d6e656d2ca1bfef939943faae9f
collins <collins@collinss-MacBook-Pro-2.local>
2017-08-01 20:12:44 -0400

    this si a git repo 7

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ app.set('port', process.env.PORT || 3000);
 // app.use(logger('dev'));
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: false }));
+app.use(function(request, response, next) {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.static('public'));
 
 // app.get('/', )
