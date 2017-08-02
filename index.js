1ee57d80ca72aad4ebe881445d1c90a0ec09a5f7
collins <collins@collinss-MacBook-Pro-2.local>
2017-08-01 21:16:28 -0400

    this si a git repo 16

--- index.js
+++ index.js
@@ -17,7 +17,11 @@ app.set('port', process.env.PORT || 3000);
 // });
 // app.use(express.static('public'));
 
-// app.get('/', )
+app.get('/', function(err, res){
+
+  res.send("this isthe server");
+  
+})
 var server = app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
